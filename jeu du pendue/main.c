a21252a288bd779e5faa5b54bbb660728ae59d1c
Fluffy-Paws881 <56996775+Fluffy-Paws881@users.noreply.github.com>
2020-11-20 12:15:01 +0100

    modification sans global

--- jeu du pendue/main.c
+++ jeu du pendue/main.c
@@ -3,21 +3,68 @@
 #include <string.h>
 #include <windows.h>
 
-char saisieInit[255], recup[255];
-int  life =9;
-int check =0;
+void *recupChaine(char* ,char*);
+void selecChar(char*);
+int verifSelect(char* ,char*,char*,int*);
 
 
-void recupChaine() //demande de la saisie du mots rechercher a l utilisateur
+int main()
 {
-    printf("saisissez un mots\n");
-    scanf("%s",saisieInit);
+    int  life =9, check =0;
+    char saisieInit[255],recup[255],carac;
 
-     for (int i = 0; i<strlen(saisieInit);i++)
+    recupChaine(&saisieInit,&recup);
+
+    int verifBool;
+
+   do
+   {
+       fflush(stdin);
+
+
+      selecChar(&carac);
+
+
+      verifBool = verifSelect(carac,saisieInit,recup,&check);
+
+      if (verifBool==0)//verifie si la lettre n'as pas etait trouvé dans la chaine
     {
-        recup[i] = '_';
+        life--;
+    }
+
+      printf("%s\n",recup);
+      printf("nombre de vie : %d\n",life);
+
+
+    //if (life == 0) break;
+   }
+   while((check !=strlen(saisieInit))&& (life != 0));
+
+
+if (life == 0)
+{
+    printf("perdue, le mots etait : %s\n",saisieInit);
+}else{
+    printf("felicitation, vous avez trouvez le mots\n");
+}
+
+
+
+    return 0;
+}
+
+void *recupChaine(char *j_saisie,char *j_recup) //demande de la saisie du mots rechercher a l utilisateur
+{
+
+    printf("saisissez un mots\n");
+    scanf("%s",j_saisie);
+
+     for (int i = 0; i<strlen(j_saisie);i++)
+    {
+        j_recup[i] = '_';
     }
     system("cls");
+    //return *j_saisie;
 }
 
 void selecChar(char *rec)//selection d'un caractere par l utilisateur
@@ -31,9 +78,9 @@ void selecChar(char *rec)//selection d'un caractere par l utilisateur
     system("cls");
 }
 
-int verifSelect(char *rec)//verifcation et assignation des bonnes lettres
+int verifSelect(char *rec,char *saisieInit,char *recup,int *j_check)//verifcation et assignation des bonnes lettres
 {
-    int checked = 0;
+    int checked = 0, compt=0;
 
     for (int i =0;i<strlen(saisieInit);i++ )
     {
@@ -41,60 +88,10 @@ int verifSelect(char *rec)//verifcation et assignation des bonnes lettres
         if (rec == saisieInit[i])
         {
             recup[i] = rec;
-            check++;
+            compt++;
             checked = 1;
         }
     }
-
-    if (checked==0)//verifie si la lettre n'as pas etait trouvé dans la chaine
-    {
-        life--;
-    }
-
+    *j_check+=compt;
     return checked;
-}
-
-
-
-int main()
-{
-    char carac;
-
-    recupChaine();
-
-    int verifBool =0;
-
-   do
-   {
-       fflush(stdin);
-
-
-      selecChar(&carac);
-
-
-      verifBool = verifSelect(carac);
-
-      printf("%s\n",recup);
-      printf("nombre de vie : %d\n",life);
-
-
-    if (life == 0) break;
-   }
-   while(check !=strlen(saisieInit));
-
-
-if (life == 0)
-{
-    printf("perdue, le mots etait : %s\n",saisieInit);
-}else{
-    printf("felicitation, vous avez trouvez le mots\n");
-}
-
-
-
-
-
-
-
-    return 0;
 }
